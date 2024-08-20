e569841b24b9d506116ecd34f3398f88444e3b20
akbay74 <asv1974@list.ru>
2024-08-20 20:19:37 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    while len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    return first
+
+print(get_multiplied_digits(40203))
